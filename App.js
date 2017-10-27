5e7ffcea5c608fcf8ec56bf280e80edb8f6d6d5f
soewufunmilola <soewufunmilola@gmail.com>
2017-10-27 12:09:07 +0100

    100% done with the login screen

--- App.js
+++ App.js
@@ -17,47 +17,82 @@ const colors = {
 }
 
 const loginButtonInfo = {
-height: 45,
-pageFontSize: 11,
-borderWidth: 0.8
+    height: 45,
+    pageFontSize: 11,
+    borderWidth: 0.8
 }
 
 const urls = {
-  forgotInstagramLogin: 'https://www.instagram.com/accounts/password/reset'
-
+    forgotInstagramLogin: 'https://www.instagram.com/accounts/password/reset',
+    twitterLoginUrl: 'https://www.twitter.com/login?lang=en',
+    instagramSignUp: 'https://www.instagram.com/accounts/emailsignup/?hl=en'
 }
 
 export default class App extends React.Component
 {
 
-  loginButtonTapped = () => {
-    console.log('the button was just tapped');
+    loginButtonTapped = () => {
+        console.log('the button was just tapped');
   }
 
 
     orSeperatorComponent = () => {
         return(
-            <View style= {viewStyles.orSeperatorComponent}>
-                <View style = {viewStyles.orSeperatorline}/>
-                <Text style= {textStyles.orSeperatorText}> OR</Text>
-                <View style = {viewStyles.orSeperatorline}/>
-            </View>
+                <View style= {viewStyles.orSeperatorComponent}>
+                    <View style = {viewStyles.orSeperatorline}/>
+                    <Text style= {textStyles.orSeperatorText}> OR</Text>
+                    <View style = {viewStyles.orSeperatorline}/>
+                </View>
         );
 
 
     }
 
+    signUpFooter = () => {
+        return(
+                <View style = {[viewStyles.forgottenLoginEncapsulationView,viewStyles.signUpFooter]}>
+                <Text style = {textStyles.forgottenLogin}> This lame nigga aint got an account??   </Text>
+                <TappableText
+                  textStyle = {[textStyles.forgottenLogin, textStyles.forgottenLoginBold]}
+                  textTapped = {() => Linking.openURL(urls.instagramSignUp)}
+                >
+                      Kneel Up!
+                </TappableText>
+                </View>
+        )
+
+    }
+
+    loginWithTwitterTappableTextComponent = () =>{
+        return(
+                 <View style = {viewStyles.twitterLoginComponentEncapsulatingView}>
+                    <Image
+                      source= {require('./src/images/twitter_bird.png')}
+                      style = {viewStyles.twitterIcon}
+                      resizeMode = {'contain'}
+                    />
+
+                  <TappableText
+                    textStyle = {textStyles.twitterLoginText}
+                    textTapped = {() => Linking.openURL(urls.twitterLoginUrl)}
+                  >
+                         Log In With Twitter
+                  </TappableText>
+                </View>
+        )
+
+    }
 
   render() {
 
     return (
         <Image source = {require('./src/images/Superman.jpg')} style = {viewStyles.container}>
             <StatusBar backgroundColor="transparent" barStyle= "dark-content"/>
-            <ScrollView>
+            <ScrollView style={viewStyles.scrollView}>
               <Image source = {require ('./src/images/instaplaylogo2.png')}
               resizeMode = {'contain'}
               style ={viewStyles.instagramTextLogo}
-            />
+              />
 
               <LoginButton
               buttonViewStyle = {viewStyles.instagramLoginButtonView}
@@ -65,7 +100,6 @@ export default class App extends React.Component
               buttonTextStyle = {{ color: colors.text}}
               buttonTapped = {this.loginButtonTapped}
               activeOpacity = {0.75}
-
               >
               Log In (Via Instagram)
               </LoginButton>
@@ -76,11 +110,9 @@ export default class App extends React.Component
               buttonTextStyle = {{ color: colors.text}}
               buttonTapped = {this.loginButtonTapped}
               activeOpacity = {0.75}
-
               >
               Facebook Login
               </LoginButton>
-
 
               <View style = {viewStyles.forgottenLoginEncapsulationView}>
               <Text style = {textStyles.forgottenLogin}> Forgotten your login details? </Text>
@@ -94,9 +126,10 @@ export default class App extends React.Component
               </TappableText>
 
               </View>
-              <this.orSeperatorComponent/>
-
+              {this.orSeperatorComponent()}
+              {this.loginWithTwitterTappableTextComponent()}
             </ScrollView>
+            {this.signUpFooter()}
        </Image>
     );
   }
@@ -107,36 +140,38 @@ export default class App extends React.Component
 
 const viewStyles = {
 
-  container: {
+   container:
+   {
       flex: 1,
       alignItems: 'center',
-      justifyContent: 'center',
       width: null,
       height: null
     },
 
+    scrollView: {
+      flex: 0.8
+    },
 
     instagramTextLogo:
     {
       width: 400,
       height:200,
-      marginTop: 10,
-      marginBottom: 30,
+      marginTop: 24,
       alignSelf: 'center'
     },
 
 
     instagramLoginButtonView:
     {
-        backgroundColor: 'transparent',
-        borderColor: colors.instagramButtonBorderColor,
-        borderWidth: 0.8,
-        borderRadius: 5,
-        width: standardComponentWidth,
-        height: loginButtonInfo.height,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 20
+      backgroundColor: 'transparent',
+      borderColor: colors.instagramButtonBorderColor,
+      borderWidth: 0.8,
+      borderRadius: 5,
+      width: standardComponentWidth,
+      height: loginButtonInfo.height,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 20
     },
 
     facebookLoginButtonView:
@@ -155,19 +190,18 @@ const viewStyles = {
 
     facebookloginButtonHighlightStyle:
     {
-        backgroundColor: colors.facebook,
-        width: standardComponentWidth,
-        height: loginButtonInfo.height,
-        marginTop: 30,
-        marginBottom: 10,
-        borderWidth: loginButtonInfo.borderWidth,
-        borderRadius: loginButtonInfo.borderRadius,
-        borderColor: colors.facebookButtonBorderColor
-
-
+      backgroundColor: colors.facebook,
+      width: standardComponentWidth,
+      height: loginButtonInfo.height,
+      marginTop: 30,
+      marginBottom: 10,
+      borderWidth: loginButtonInfo.borderWidth,
+      borderRadius: loginButtonInfo.borderRadius,
+      borderColor: colors.facebookButtonBorderColor
     },
 
-    forgottenLoginEncapsulationView:{
+    forgottenLoginEncapsulationView:
+    {
       flexDirection: 'row',
       justifyContent: 'center',
       alignItems: 'center',
@@ -176,50 +210,95 @@ const viewStyles = {
       marginBottom: 10
     },
 
-    orSeperatorComponent: {
-        flexDirection: 'row',
-        width: standardComponentWidth,
-        paddingHorizontal: 10,
-        paddingVertical: 2,
-        marginVertical: 10,
-        alignItems: 'center',
-        alignSelf: 'center'
+    orSeperatorComponent:
+    {
+      flexDirection: 'row',
+      width: standardComponentWidth,
+      paddingHorizontal: 10,
+      paddingVertical: 2,
+      marginVertical: 10,
+      alignItems: 'center',
+      alignSelf: 'center'
 
     },
 
-    orSeperatorline: {
+    orSeperatorline:
+    {
+      height: 1,
+      flex: 5,
+      backgroundColor: colors.facebookButtonBorderColor,
+      borderColor: colors.facebookButtonBorderColor,
+      borderWidth: 0.5,
+    },
 
-        height: 1,
-        flex: 5,
-        backgroundColor: colors.facebookButtonBorderColor,
-        borderColor: colors.facebookButtonBorderColor,
-        borderWidth: 0.5,
+    twitterLoginComponentEncapsulatingView:
+    {
+      flexDirection: 'row',
+      paddingHorizontal: 10,
+      width: standardComponentWidth,
+      height: 100,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: 11,
+      alignSelf: 'center'
+    },
 
+    twitterIcon:
+    {
+
+      width: 20,
+      height: 20
+    },
+
+    signUpFooter:
+    {
+      flex: 0.092,
+      backgroundColor: 'rgba(255, 255, 255, 0.15)',
+      borderWidth: 1,
+      borderColor: 'rgba(255, 255, 255, 0.15)',
+      shadowColor: 'black',
+      shadowOffset: {width: 0, height: 1},
+      top: 10,
+      elevation: 2,
+      height: null,
+      width: '100%'
 
     }
+
 
 };
 
 const textStyles =
 {
-  forgottenLogin:
-  {
-    color:'white',
-    fontSize: loginButtonInfo.pageFontSize,
-  },
+    forgottenLogin:
+    {
+        color:'white',
+        fontSize: loginButtonInfo.pageFontSize,
+    },
 
-  forgottenLoginBold:
-  {
-      fontWeight: 'bold'
-  },
+    forgottenLoginBold:
+    {
+        fontWeight: 'bold'
+    },
 
-  orSeperatorText:
-  {
-    fontWeight: 'bold',
-    fontSize: 12.5,
-    marginHorizontal: 3,
-    flex: 0.7,
-    color: 'white'
-  }
+    orSeperatorText:
+    {
+        fontWeight: 'bold',
+        fontSize: 12.5,
+        marginHorizontal: 3,
+        flex: 0.7,
+        color: 'white'
+    },
+
+    twitterLoginText:
+    {
+        marginLeft: 5,
+        fontWeight: 'bold',
+        fontSize: 13,
+        color: 'white',
+        backgroundColor: 'transparent'
+    }
+
+
 
 };
